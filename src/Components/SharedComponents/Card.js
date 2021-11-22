5d9d6eccf9896e6ba2d0b2c80b39b0e0cc25632a
tarunsai217 <tarunsai217@gmail.com>
2021-11-22 23:05:26 +0530

    Final Commit

--- src/Components/SharedComponents/Card.js
+++ src/Components/SharedComponents/Card.js
@@ -4,22 +4,24 @@ import styles from './Card.module.css'
 function Card({right,title}) {
     const leftCard={
        textAlign:'right',
-       paddingLeft:'30px'
+       paddingRight:'50px',
+       marginLeft:'30px'       
     }
 
     const rightCard={
-    textAlign:'left',
-    paddingRight:'30px'
+       textAlign:'left',
+       paddingLeft:'50px',
+       marginRight:'30px'
     }
 
     return (
         <div className={styles.card}>
-            {right && <div style={{paddingRight:'15px'}} className={styles.imageContainer}> <img alt='plant' src='https://live-demo.wooskins.com/themes/ws-juice/wp-content/uploads/2020/02/service-icon2.png'></img></div>}
+            {right && <div  className={styles.imageContainer}> <img alt='plant' src='https://live-demo.wooskins.com/themes/ws-juice/wp-content/uploads/2020/02/service-icon2.png'></img></div>}
             <div style={right?rightCard:leftCard} className={styles.textContainer}>
                 <h3>{title}</h3>
                 <p>Lorem ipsum dolor sit amet, conc Lorem sit amet, concLorem ipsum dolor</p>
             </div>
-            {!right && <div style={{paddingLeft:'25px'}} className={styles.imageContainer}> <img alt='plant' src='https://live-demo.wooskins.com/themes/ws-juice/wp-content/uploads/2020/02/service-icon2.png'></img></div>}
+            {!right && <div  className={styles.imageContainer}> <img alt='plant' src='https://live-demo.wooskins.com/themes/ws-juice/wp-content/uploads/2020/02/service-icon2.png'></img></div>}
         </div>
     )
 }
--- src/Components/SharedComponents/Dropdown/Dropdown.js
+++ src/Components/SharedComponents/Dropdown/Dropdown.js
@@ -8,4 +8,5 @@ function Dropdown({data}) {
     )
 }
 
+
 export default Dropdown
